cad63030d3e63ab75ec9afba0d46ad6b5b2f004a
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2023-02-11 22:17:10 +0300

    Initial Commit

---
+++ MyCalculatorUI/MyCalculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorUIApp.swift
+//  MyCalculatorUI
+//
+//  Created by Andrei Dokuchaev on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
